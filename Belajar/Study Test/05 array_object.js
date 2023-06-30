c2c0a73d611c8e5f61dc96dc112e534928df76aa
Sandra Marsella <sandra03marsella@gmail.com>
2023-06-30 17:19:36 +0700

    test

--- Belajar/Study Test/05 array_object.js
+++
@@ -1,4 +0,0 @@
-const array_1 = ["A", "B", "C", "A"];
-
-const nilaiC = array_1[2];
-console.log(nialiC2);
---
+++ Belajar/Study Test/05_array_object.js
@@ -0,0 +1,4 @@
+const array_1 = ["A", "B", "C", "A"];
+
+const nilaiC = array_1[0];
+console.log(nilaiC);
